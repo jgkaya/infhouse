9b1ce9586495dbd649ca9aaf9a8f8ac7500835f7
Sıla Soyat <sila.soyat10@gmail.com>
2026-01-02 13:42:18 +0300

    pages seperated

---
+++ app/markalar/page.tsx
@@ -0,0 +1,11 @@
+import Header from "@/components/Header";
+import CarouselInf from "@/components/pages/CarouselInf";
+
+export default function MarkalarPage() {
+    return (
+        <main className="bg-white min-h-screen">
+            <Header />
+            <CarouselInf />
+        </main>
+    );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,6 @@ import Header from "@/components/Header";
 import HomeHero from "@/components/pages/Home/HomeHero";
 import StatsShowcase from "@/components/pages/Home/StatsShowcase";
 import WhatWeDo from "@/components/pages/Home/WhatWeDo";
-import { WorkHero } from "@/components/pages/WorkHero";
-import CarouselInf from "@/components/pages/CarouselInf";
 
 export default function Page() {
   return (
@@ -13,14 +11,6 @@ export default function Page() {
 
       <WhatWeDo />
       <StatsShowcase />
-
-      <section id="calismalarimiz">
-        <WorkHero />
-      </section>
-
-      <section id="markalar">
-        <CarouselInf />
-      </section>
     </main>
   );
 }
